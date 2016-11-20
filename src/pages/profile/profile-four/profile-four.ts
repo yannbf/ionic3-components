50b4bfad714ffa2be88e904b264043a94fabdedd
Yann Braga <yannbf@gmail.com>
2016-11-20 18:41:55 -0300

    New profile page

--- src/pages/profile/profile-four/profile-four.ts
+++ src/pages/profile/profile-four/profile-four.ts
@@ -1,3 +1,5 @@
+import { ToastService } from '../../../providers/util/toast.service';
+import { AlertService } from '../../../providers/util/alert.service';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,10 +9,65 @@ import { NavController } from 'ionic-angular';
 })
 export class ProfileFourPage {
 
-  constructor(public navCtrl: NavController) { }
+  following = false;
+  user = {
+    name: 'Paula Bolliger',
+    profileImage: 'assets/img/avatar/girl-avatar.png',
+    coverImage: 'assets/img/background/background-5.jpg',
+    occupation: 'Designer',
+    location: 'Seattle, WA',
+    description: 'A wise man once said: The more you do something, the better you will become at it.',
+    followers: 456,
+    following: 1052,
+    posts: 35
+  }
+
+  posts = [
+    {
+      postImageUrl: 'assets/img/background/background-2.jpg',
+      text: 'I believe in being strong when everything seems to be going wrong. I believe that happy girls are the prettiest girls. I believe that tomorrow is another day and I believe in miracles.',
+      date: 'November 5, 2016',
+      likes: 12,
+      comments: 4,
+      timestamp: '11h ago'
+    },
+    {
+      postImageUrl: 'assets/img/background/background-3.jpg',
+      text: 'Do not go where the path may lead, go instead where there is no path and leave a trail.',
+      date: 'October 23, 2016',
+      likes: 30,
+      comments: 64,
+      timestamp: '30d ago'
+    },
+    {
+      postImageUrl: 'assets/img/background/background-4.jpg',
+      date: 'June 28, 2016',
+      likes: 46,
+      text: 'Hope is the thing with feathers that perches in the soul And sings the tune without the words And never stops at all.',
+      comments: 66,
+      timestamp: '4mo ago'
+    },
+  ];
+  constructor(public navCtrl: NavController, public toastCtrl: ToastService) { }
 
   ionViewDidLoad() {
     console.log('Hello ProfileFour Page');
+  }
+  follow() {
+    this.following = !this.following;
+    this.toastCtrl.create('Follow user clicked');
+  }
+
+  imageTapped(post) {
+    this.toastCtrl.create('Post image clicked');
+  }
+
+  comment(post) {
+    this.toastCtrl.create('Comments clicked');
+  }
+
+  like(post) {
+    this.toastCtrl.create('Like clicked');
   }
 
 }
