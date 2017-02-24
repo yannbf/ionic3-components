39197f6012b04ab13645b278d14d60901355465f
Yann Braga <yannbf@gmail.com>
2017-02-24 01:22:51 -0300

    resolves #2

--- src/pages/miscellaneous/runtime-permissions/README.md
+++ src/pages/miscellaneous/runtime-permissions/README.md
@@ -1,3 +1,4 @@
+On newer android versions, permissions are handled differently. In most of the cases, the plugins take care of the permissions, but sometimes there are some exceptions.
 In order to ask or check for user permissions, the Diagnostic plugin is needed.
 
 Docs: https://ionicframework.com/docs/v2/native/diagnostic/
@@ -5,6 +6,3 @@ Docs: https://ionicframework.com/docs/v2/native/diagnostic/
 You have to install the plugin with:
 ```sh
 $ ionic plugin add cordova.plugins.diagnostic --save
-```
-
-
--- src/pages/miscellaneous/runtime-permissions/runtime-permissions.ts
+++ src/pages/miscellaneous/runtime-permissions/runtime-permissions.ts
@@ -8,48 +8,53 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class RuntimePermissionsPage {
 
+  // You can add many other permissions
   PERMISSION = {
     WRITE_EXTERNAL: Diagnostic.permission.WRITE_EXTERNAL_STORAGE,
     READ_EXTERNAL: Diagnostic.permission.READ_EXTERNAL_STORAGE,
     CAMERA: Diagnostic.permission.CAMERA,
-    READ_CONTACTS: Diagnostic.permission.READ_CONTACTS,
-    RECORD_AUDIO: Diagnostic.permission.RECORD_AUDIO,
-    CALL_PHONE: Diagnostic.permission.CALL_PHONE,
   };
 
   constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
+  // You can use this kind of method, which is passing a permission value..
   requestPermission(permission) {
-    Diagnostic.requestRuntimePermission(permission).then(() => {
-      console.log('permission granted');
+    Diagnostic.requestRuntimePermission(permission).then((status) => {
+      if (status == Diagnostic.permissionStatus.GRANTED) {
+        alert('Permission granted!');
+      } else {
+        alert('Permission not granted. STATUS: ' + status);
+      }
     }, error => {
       console.error('permission error:', error);
     });
   }
 
-  requestAllPermissions() {
-    var permissions = Object.keys(this.PERMISSION).map(k => this.PERMISSION[k]);
-    Diagnostic.requestRuntimePermissions(permissions).then(() => {
-      console.log('permission granted');
-    }, error => {
-      console.error('permission error:', error);
-    });
-  }
-
-  funca() {
+  // ..Or opt for Diagnostic's specific methods, like requestCameraAuthorization.
+  requestCameraPermission() {
+    // Checks if permission is already granted. Otherwise, asks for it.
     Diagnostic.isCameraAuthorized().then((authorized) => {
       if (authorized) {
+        alert('camera is already authorized!');
       } else {
-        Diagnostic.requestContactsAuthorization
-        Diagnostic.requestContactsAuthorization().then((status) => {
+        Diagnostic.requestCameraAuthorization().then((status) => {
           if (status == Diagnostic.permissionStatus.GRANTED) {
-
+            alert('Permission granted!');
           } else {
-            // Permissions not granted
+            alert('Permission not granted. STATUS: ' + status);
           }
         });
       }
     });
   }
 
+  // There is also a method that takes an array of permissions to ask for them at once
+  requestAllPermissions() {
+    var permissions = Object.keys(this.PERMISSION).map(k => this.PERMISSION[k]);
+    Diagnostic.requestRuntimePermissions(permissions).then((status) => {
+      alert(JSON.stringify(status));
+    }, error => {
+      console.error('permission error:', error);
+    });
+  }
 }
