f641aa33665d4be6507e99f01290635b9ceee192
Yann Braga <yannbf@gmail.com>
2017-06-28 00:20:18 -0300

    fix(messages): Add missing shared module

--- src/pages/miscellaneous/chat/messages/messages.module.ts
+++ src/pages/miscellaneous/chat/messages/messages.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from '../../../../app/shared.module';
 import { MessagesPage } from './messages';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
@@ -8,6 +9,7 @@ import { IonicPageModule } from 'ionic-angular';
     ],
     imports: [
         IonicPageModule.forChild(MessagesPage),
+        SharedModule,
     ],
     exports: [
         MessagesPage
