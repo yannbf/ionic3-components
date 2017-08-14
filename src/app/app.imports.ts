b6b5b66546030bdcee09565db2253c5d0760cf28
Damodar Lohani <dlohani48@gmail.com>
2017-08-14 18:15:58 +0545

    imported timeline-item component

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -27,6 +27,9 @@ import { TimerProgress } from '../components/timer-progress/timer-progress';
 import { ExpandableHeader } from '../components/expandable-header/expandable-header';
 import { FlashCardComponent } from '../components/flash-card/flash-card';
 import { AccordionListComponent } from '../components/accordion-list/accordion-list';
+import { TimelineComponent } from '../components/timeline/timeline';
+import { TimelineItemComponent } from '../components/timeline/timeline';
+
 
 // Pipes
 import { MomentPipe } from '../pipes/moment.pipe';
@@ -75,6 +78,8 @@ export const COMPONENTS = [
   ExpandableHeader,
   Autosize,
   AccordionListComponent,
+  TimelineComponent,
+  TimelineItemComponent
 ];
 
 export const DIRECTIVES = [
