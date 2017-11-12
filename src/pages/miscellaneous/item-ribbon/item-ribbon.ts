c07458eb42ca4864cfa553857e2f12ec925adf5b
Yann Braga <yannbraga@Yanns-MacBook-Pro.local>
2017-11-12 17:05:55 -0300

    refactor(item-ribbon): remove unnecessary comments

--- src/pages/miscellaneous/item-ribbon/item-ribbon.ts
+++ src/pages/miscellaneous/item-ribbon/item-ribbon.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ItemRibbonPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-item-ribbon',
@@ -15,7 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ItemRibbonPage {
 
-  public thumbnails: {title: string, image: string, ribbonText: string, class: string}[] = [
+  public thumbnails: { title: string, image: string, ribbonText: string, class: string }[] = [
     { title: 'Awesome Shoes - $9', image: 'assets/img/ribbon/shoes.jpg', ribbonText: 'sale', class: 'sale' },
     { title: 'Awesome Team', image: 'assets/img/ribbon/cruzeiro.jpg', ribbonText: 'awesome', class: 'cruzeiro' },
     { title: 'Bad team', image: 'assets/img/ribbon/atletico.jpg', ribbonText: 'bad', class: 'atletico' }
@@ -23,11 +16,4 @@ export class ItemRibbonPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ItemRibbonPage');
-  }
-
 }
