461fa877ca2072d6df8caab9d3541ed3c3715f45
Mateus Duraes <mduraes1994@gmail.com>
2017-11-12 19:04:27 -0200

    feat(component): ion-item with ribbon (#56)
    * Ion itens with side ribbon
    
    * fix text

---
+++ src/pages/miscellaneous/item-ribbon/README.md
@@ -0,0 +1,1 @@
+It was inspired by cards in the [@willianJusten](https://github.com/willianjusten) [blog](https://willianjusten.com.br/), and, adapted to Ionic.
---
+++ src/pages/miscellaneous/item-ribbon/item-ribbon.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ItemRibbonPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-item-ribbon',
+  templateUrl: 'item-ribbon.html',
+})
+export class ItemRibbonPage {
+
+  public thumbnails: {title: string, image: string, ribbonText: string, class: string}[] = [
+    { title: 'Awesome Shoes - $9', image: 'assets/img/ribbon/shoes.jpg', ribbonText: 'sale', class: 'sale' },
+    { title: 'Awesome Team', image: 'assets/img/ribbon/cruzeiro.jpg', ribbonText: 'awesome', class: 'cruzeiro' },
+    { title: 'Bad team', image: 'assets/img/ribbon/atletico.jpg', ribbonText: 'bad', class: 'atletico' }
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ItemRibbonPage');
+  }
+
+}
