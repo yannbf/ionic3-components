e5e5a70171e8c5bd5dd2132de46a3dee4374ccc8
mduraes1994 <mduraes1994@gmail.com>
2017-11-12 14:48:34 -0200

    fix text

--- src/pages/miscellaneous/item-ribbon/item-ribbon.ts
+++ src/pages/miscellaneous/item-ribbon/item-ribbon.ts
@@ -17,7 +17,7 @@ export class ItemRibbonPage {
 
   public thumbnails: {title: string, image: string, ribbonText: string, class: string}[] = [
     { title: 'Awesome Shoes - $9', image: 'assets/img/ribbon/shoes.jpg', ribbonText: 'sale', class: 'sale' },
-    { title: 'Awesome Tean', image: 'assets/img/ribbon/cruzeiro.jpg', ribbonText: 'awesome', class: 'cruzeiro' },
+    { title: 'Awesome Team', image: 'assets/img/ribbon/cruzeiro.jpg', ribbonText: 'awesome', class: 'cruzeiro' },
     { title: 'Bad team', image: 'assets/img/ribbon/atletico.jpg', ribbonText: 'bad', class: 'atletico' }
   ];
 
