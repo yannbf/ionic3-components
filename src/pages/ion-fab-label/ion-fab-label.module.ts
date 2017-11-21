05bc34f84c0766e49fcce29884d416b82588b400
Junior Abranches <jr-abranches@hotmail.com>
2017-11-21 17:41:38 -0300

    Added ionFab-Label

---
+++ src/pages/ion-fab-label/ion-fab-label.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { IonFabLabelPage } from './ion-fab-label';
+
+@NgModule({
+  declarations: [
+    IonFabLabelPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(IonFabLabelPage),
+  ],
+})
+export class IonFabLabelPageModule {}
